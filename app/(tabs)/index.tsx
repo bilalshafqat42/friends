203d8a559d0e48cc5419f40923efcc093caf62a9
Bilal Shafqat <bilalshafqat@Bilals-MacBook-Pro.local>
2025-05-19 15:29:03 +0400

    home screen done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,6 @@
 import { ImageBackground } from "expo-image";
 import React from "react";
-import { Image, StyleSheet, Text } from "react-native";
-
+import { Image, StyleSheet } from "react-native";
 
 const HomeScreen = () => {
   return (
@@ -10,8 +9,14 @@ const HomeScreen = () => {
       contentFit="cover"
       source={require("../../assets/images/newBg.jpg")}
     >
-      <Image source={require("../../assets/images/friends.png")} width={300}/>
-      <Text>index</Text>
+      <Image
+        source={require("../../assets/images/friends.png")}
+        style={styles.logoImage}
+      />
+      <Image
+        source={require("../../assets/images/play.png")}
+        style={styles.playButtonImage}
+      />
     </ImageBackground>
   );
 };
@@ -24,7 +29,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     alignSelf: "center",
-    width: "100%"
+    width: "100%",
   },
   overlay: {
     backgroundColor: "rgba(0,0,0,0.3)",
@@ -32,5 +37,14 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     width: "100%", // makes it take full width of parent
     alignItems: "center", // center text inside
+  },
+  logoImage: {
+    width: 376,
+    height: 220,
+    marginBottom: 20
+  },
+  playButtonImage: {
+    width: 135,
+    height: 97,
   },
 });
