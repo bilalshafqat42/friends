c28c961302ea14dc6122bb72b153f4eb1e32bad0
Bilal Shafqat <bilalshafqat@Bilals-MacBook-Pro.local>
2025-05-20 16:43:45 +0400

    new updates

--- app/(learning)/learning.tsx
+++ app/(learning)/learning.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 import { ImageBackground } from "expo-image";
 import TopNavigation from "@/components/TopNavigation";
 import BottomNavigation from "@/components/BottomNavigation";
@@ -8,47 +15,68 @@ import LearnComponent from "@/components/Learn";
 import { FlatList } from "react-native-gesture-handler";
 
 const LearningScreen = () => {
-  const myData = [
-    { id: 1, name: "Bilal", desc: "welcome i am bilal the developer" },
-    { id: 2, name: "Bilal", desc: "welcome i am bilal the developer" },
-    { id: 3, name: "Bilal", desc: "welcome i am bilal the developer" },
-    { id: 4, name: "Bilal", desc: "welcome i am bilal the developer" },
-    { id: 5, name: "Bilal", desc: "welcome i am bilal the developer" },
-    { id: 6, name: "Bilal", desc: "welcome i am bilal the developer" },
-    { id: 7, name: "Bilal", desc: "welcome i am bilal the developer" },
-    { id: 8, name: "Bilal", desc: "welcome i am bilal the developer" },
-    { id: 9, name: "Bilal", desc: "welcome i am bilal the developer" },
-    { id: 10, name: "Bilal", desc: "welcome i am bilal the developer" },
-    { id: 11, name: "Bilal", desc: "welcome i am bilal the developer" },
-    { id: 12, name: "Bilal", desc: "welcome i am bilal the developer" },
-    { id: 13, name: "Bilal", desc: "welcome i am bilal the developer" },
-    { id: 14, name: "Bilal", desc: "welcome i am bilal the developer" },
-    { id: 15, name: "Bilal", desc: "welcome i am bilal the developer" },
-    { id: 16, name: "Bilal", desc: "welcome i am bilal the developer" },
-    { id: 17, name: "Bilal", desc: "welcome i am bilal the developer" },
-    { id: 18, name: "Bilal", desc: "welcome i am bilal the developer" },
-  ];
+  // const myData = [
+  //   { id: 1, name: "Bilal", desc: "welcome i am bilal the developer" },
+  //   { id: 2, name: "Bilal", desc: "welcome i am bilal the developer" },
+  //   { id: 3, name: "Bilal", desc: "welcome i am bilal the developer" },
+  //   { id: 4, name: "Bilal", desc: "welcome i am bilal the developer" },
+  //   { id: 5, name: "Bilal", desc: "welcome i am bilal the developer" },
+  //   { id: 6, name: "Bilal", desc: "welcome i am bilal the developer" },
+  //   { id: 7, name: "Bilal", desc: "welcome i am bilal the developer" },
+  //   { id: 8, name: "Bilal", desc: "welcome i am bilal the developer" },
+  //   { id: 9, name: "Bilal", desc: "welcome i am bilal the developer" },
+  //   { id: 10, name: "Bilal", desc: "welcome i am bilal the developer" },
+  //   { id: 11, name: "Bilal", desc: "welcome i am bilal the developer" },
+  //   { id: 12, name: "Bilal", desc: "welcome i am bilal the developer" },
+  //   { id: 13, name: "Bilal", desc: "welcome i am bilal the developer" },
+  //   { id: 14, name: "Bilal", desc: "welcome i am bilal the developer" },
+  //   { id: 15, name: "Bilal", desc: "welcome i am bilal the developer" },
+  //   { id: 16, name: "Bilal", desc: "welcome i am bilal the developer" },
+  //   { id: 17, name: "Bilal", desc: "welcome i am bilal the developer" },
+  //   { id: 18, name: "Bilal", desc: "welcome i am bilal the developer" },
+  // ];
   return (
     <ImageBackground
       source={require("../../assets/images/welcome.png")}
       style={styles.learningScreen}
     >
       <TopNavigation />
-      <View style={styles.learningScreen}>
-        <Text>Learning</Text>
+      <View style={styles.singleLearning}>
+        <Image
+          source={require("../../assets/images/learning/ant.png")}
+          style={styles.singleLearningImage}
+        />
+        <Text style={styles.headingSingle}>ANT</Text>
+        <TouchableOpacity style={styles.playButton}>
+          <Text style={styles.singleButtonText}>Play</Text>
+          <Image
+            source={require("../../assets/images/learning/voice.png")}
+            style={{ width: 54, height: 42 }}
+          />
+        </TouchableOpacity>
+      </View>
+      {/* <View style={styles.learningScreen}>
         <FlatList
-          data={myData}
+          data={spells}
           keyExtractor={(item) => item.id.toString()}
           renderItem={({ item, index }) => (
             <View key={index} style={styles.miniContainer}>
               <Text style={styles.heading}>
                 {item.id} - {item.name}
               </Text>
-              <Text style={styles.normalText}>{item.desc}</Text>
+              <Image source={item.image} style={{ width: 100, height: 100 }} />
+              <Button title="Play" onPress={() => alert("Button Pressed")} />
+              <TouchableOpacity style={styles.playButton}>
+                <Text style={styles.singleButtonText}>Play</Text>
+                <Image
+                  source={item.iconImage}
+                  style={{ width: 30, height: 30 }}
+                />
+              </TouchableOpacity>
             </View>
           )}
         />
-      </View>
+      </View> */}
       <BottomNavigation />
     </ImageBackground>
   );
@@ -67,12 +95,22 @@ const styles = StyleSheet.create({
   //   flex: 1,
   //   backgroundColor: "#000",
   // },
-  miniContainer: {
-    padding: 20,
-    margin: 20,
-    backgroundColor: "#fff",
-    borderRadius: 10,
-    rowGap: 5,
+  singleLearningImage: {
+    width: 180,
+    height: 180,
+  },
+  headingSingle: {
+    fontSize: 70,
+    fontFamily: "bubbles",
+    textAlign: "center",
+  },
+  miniContainer: {},
+  singleLearning: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
   },
   heading: {
     fontSize: 18,
@@ -82,5 +120,22 @@ const styles = StyleSheet.create({
   },
   normalText: {
     fontSize: 16,
+  },
+  playButton: {
+    backgroundColor: "#FF049B",
+    color: "#fff",
+    borderRadius: 45,
+    width: 286,
+    height: 88,
+    textAlign: "center",
+    flex: 0,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    columnGap: 10,
+  },
+  singleButtonText: {
+    color: "#fff",
+    fontSize: 75,
   },
 });
--- assets/db/spell.ts
+++ assets/db/spell.ts
@@ -2,18 +2,55 @@ export const spells = [
   {
     id: 1,
     name: "Ant",
-    //  image: require("../images/learning/ant.png")
+    image: require("../images/learning/ant.png"),
+    iconImage: require("../images/learning/voice.png"),
   },
   {
     id: 2,
     name: "Ball",
-    // image: require("../images/ball.png")
+    image: require("../images/learning/ball.png"),
+    iconImage: require("../images/learning/voice.png"),
   },
-  // { id: 3, name: "Car", image: require("../images/car.png") },
-  // { id: 4, name: "Corn", image: require("../images/ant.png") },
-  // { id: 5, name: "Cat", image: require("../images/ant.png") },
-  // { id: 6, name: "Egg", image: require("../images/ant.png") },
-  // { id: 7, name: "Fish", image: require("../images/ant.png") },
-  // { id: 8, name: "Rose", image: require("../images/ant.png") },
-  // { id: 9, name: "Star", image: require("../images/ant.png") },
+  {
+    id: 3,
+    name: "Car",
+    image: require("../images/learning/ball.png"),
+    iconImage: require("../images/learning/voice.png"),
+  },
+  {
+    id: 4,
+    name: "Corn",
+    image: require("../images/learning/ball.png"),
+    iconImage: require("../images/learning/voice.png"),
+  },
+  {
+    id: 5,
+    name: "Cat",
+    image: require("../images/learning/ball.png"),
+    iconImage: require("../images/learning/voice.png"),
+  },
+  {
+    id: 6,
+    name: "Egg",
+    image: require("../images/learning/ball.png"),
+    iconImage: require("../images/learning/voice.png"),
+  },
+  {
+    id: 7,
+    name: "Fish",
+    image: require("../images/learning/ball.png"),
+    iconImage: require("../images/learning/voice.png"),
+  },
+  {
+    id: 8,
+    name: "Rose",
+    image: require("../images/learning/ball.png"),
+    iconImage: require("../images/learning/voice.png"),
+  },
+  {
+    id: 9,
+    name: "Star",
+    image: require("../images/learning/ball.png"),
+    iconImage: require("../images/learning/voice.png"),
+  },
 ];
